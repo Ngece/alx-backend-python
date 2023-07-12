84a7ac43d53f01a9432bbcbc7c2982423c0a056d
josaya <josaya@josaya.josaya>
2023-07-12 21:59:51 +0200

    committing sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module contains a function that takes a list of floats and integers and returns their sum as a float """
+from typing import List, Union
+
+def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
+    """ Returns the sum of the list of floats """
+    return sum(input_list)
