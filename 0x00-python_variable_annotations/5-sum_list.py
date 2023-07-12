c5762993ec758e42cff3292c3d0b7dae45433c43
josaya <josaya@josaya.josaya>
2023-07-12 21:52:14 +0200

    committing sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module contains a function that takes a list of floats and returns their sum as a float """
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """ Returns the sum of the list of floats """
+    return sum(input_list)
