b542e276f7b3b54cf388ed2d47fb854ffa20bb01
josaya <josaya@josaya.josaya>
2023-07-12 21:34:22 +0200

    committing concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,4 +2,5 @@
 """ This module contains a function that concatenates two strings """
 
 def concat(str1: str, str2: str) -> str:
+        """ Returns the concatenation of two strings"""
         return str1 + str2
