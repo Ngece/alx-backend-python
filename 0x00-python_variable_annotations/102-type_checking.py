b16ce77f4d464a2cb57231791dc1e4516b671989
josaya <josaya@josaya.josaya>
2023-07-12 23:38:28 +0200

    committing type checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" This module contains a function that uses mypy to validate some code """
+
+from typing import Union, Any, Mapping, Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    ''' Variable Annotation for list '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, int(3.0))
