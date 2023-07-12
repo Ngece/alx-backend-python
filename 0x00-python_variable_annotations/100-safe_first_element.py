cb68f9807c7bb4ea98f10e484011580ee7f9f67e
josaya <josaya@josaya.josaya>
2023-07-12 23:15:20 +0200

    committing safe first element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module contains a type-annotated function that takes a list input_list of floats as argument and returns their sum as a float. """
+
+from typing import List, Any, Union
+
+def safe_first_element(lst: List[Any]) -> Union[Any, None]:
+    """ Returns the first element of a list """
+    if lst:
+        return lst[0]
+    else:
+        return None
