ad21dc3f2c5c68077f997c360b11dfbd8ad9793d
josaya <josaya@josaya.josaya>
2023-07-12 21:28:38 +0200

    committing add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" This module contains a function that adds two integers """
+
+def add(a: float, b: float) -> float:
+    """ This function adds two integers and returns the result """
+    return a + b
