34e5c85378de33ceb986a5f671dc7711f75383e8
josaya <josaya@josaya.josaya>
2023-07-12 21:41:27 +0200

    committing floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import math
 """ This module contains a function that returns a floor """
-
 def floor(n: float) -> int:
     """ Returns the floor of a given number """
     return math.floor(n)
